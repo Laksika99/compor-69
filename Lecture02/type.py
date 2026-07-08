8a589abdc49c8a77a6134f34112e4cd678af5bf9
Laksika99 <s6806022610241@email.kmutnb.ac.th>
2026-07-08 09:51:19 +0700

    08/07

--- Lecture02/type.py
+++ Lecture02/type.py
@@ -11,4 +11,13 @@ name = "milk"
 number = int(name)
 print("Number", number)
 
+first_name = input("Enter your first name: ")
+print("First Name", first_name)
 
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+income = float(input("Enter your income: "))
+print("Here is the data you entered:")
+print("Name:", name)
+print("Age:", age)
+print("Income:", income)
