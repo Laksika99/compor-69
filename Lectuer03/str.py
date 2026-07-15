a6ec160b8b0e048f9e873d91bdedd38c3b47785f
Laksika99 <s6806022610241@email.kmutnb.ac.th>
2026-07-15 09:55:50 +0700

    9:55

--- Lectuer03/str.py
+++ Lectuer03/str.py
@@ -5,14 +5,14 @@ string2 = "Mark"
 
 #Compare the strings for equality
 if stringl == string2:
-    print(f'"(stringl)" and "(string2)" are equal.')
+    print(f'"{stringl}" and "{string2}" are equal.')
 else:
-    print(f'"(stringl)" and "(string2)" are not equal.')
+    print(f'"{stringl}" and "{string2}" are not equal.')
 #Lexicographical comparison
 if stringl < string2:
-    print(f'"(stringl)" comes before "(string2)" inlexicographical order.')
+    print(f'"{stringl}" comes before "{string2}" inlexicographical order.')
 elif stringl > string2:
-    print(f'"(stringl)" comes after "(string2)" inlexicographical order. ')
+    print(f'"{stringl}" comes after "{string2}" inlexicographical order. ')
 #Case-insensitive comparison
 if stringl.lower() == string2.lower():
     print(f'"{stringl}" and "{string2}" are equal when case is ignored.')
