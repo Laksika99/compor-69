e51b27f7a98b1faf60083aa7d2bea460d50ea574
Laksika99 <s6806022610241@email.kmutnb.ac.th>
2026-08-03 17:43:33 +0700

    CW5

---
+++ Lectuer05/varipara.py
@@ -0,0 +1,8 @@
+def find_max(*args):
+    if not args:
+        return None
+    maximum = args[0]
+    for num in args:
+        if number > max_value:
+            max_value = number
+    return max_value
