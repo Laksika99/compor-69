975ae985b82a58825bc272396994ba673ef90b33
Laksika99 <s6806022610241@email.kmutnb.ac.th>
2026-08-08 23:41:29 +0700

    jeep

--- HW-Com/hw02.py
+++ HW-Com/hw02.py
@@ -1,26 +1,26 @@
 def longest_unique_word_sequence(words: list[list[str]]) -> tuple:
     #your code here
     continuous = sum(words, [])
-    seen = [] #เก็บค่าของคำที่เจอแล้ว
-    component = [] #เก็บค่าของคำที่เจอแล้วในแต่ละ component
+    meet = [] #เก็บค่าของคำที่เจอแล้ว
+    Box = [] #เก็บค่าของคำที่เจอแล้วในแต่ละ component
     start = 0 #เก็บตำแหน่งเริ่มต้นของลำดับคำที่ไม่มีตัวซ้ำ
     for i in range(len(continuous)): #คือเพื่อวนลูปเช็คค่าของ continuous
         x = continuous[i]
        
-        if x not in seen: #คือเช็คว่าค่าของ x นั้นอยู่ใน seen มั้ย ถ้าไม่อยู่ก็ให้เพิ่มค่า x เข้าไปใน seen
-            seen.append(x) 
+        if x not in meet: #เช็คว่าค่าของ x นั้นอยู่ใน meet มั้ย ถ้าไม่อยู่ก็ให้เพิ่มค่า x เข้าไปใน meet
+            meet.append(x) 
            
-        else:
+        else:  #เช็คว่าค่าของ x นั้นอยู่ใน meet มั้ย ถ้าอยู่ก็ให้เพิ่มค่า start ขึ้น 1 และเพิ่มค่าของ meet เข้าไปใน Box
             start += 1
-            component.append(seen) #คือเพิ่มค่าของ seen เข้าไปใน component
+            Box.append(meet) #คือเพิ่มค่าของ meet เข้าไปใน Box
             
-            if x in continuous[start:i-1]: 
+            if x in continuous[start:i-1]:  #เช็คว่าค่าของ x นั้นอยู่ใน continuous[start:i-1] มั้ย ถ้าอยู่ก็ให้เพิ่มค่า start ขึ้น 1 และเพิ่มค่าของ meet เข้าไปใน Box
                 start += 1 
-                seen = continuous[start:i+1]
+                meet = continuous[start:i+1]
                 continue
-            seen = continuous[start:i+1]
-    component.append(seen)
-    final = [x for x in component if len(x) == max(len(y) for y in component)]
+            meet = continuous[start:i+1]
+    Box.append(meet)
+    final = [x for x in Box if len(x) == max(len(y) for y in Box)]
     return (len(final[0]), final)
 
 words = [["apple", "banana"], ["apple"], ["cherry", "banana"]]
